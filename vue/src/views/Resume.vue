d4d990e7c6c02cb6942bbe901e6f6ef096efcbde
Yuanzhe Liu <yul156@ucsd.edu>
2022-08-14 11:05:24 -0700

    update resume

--- vue/src/views/Resume.vue
+++ vue/src/views/Resume.vue
@@ -1,7 +1,208 @@
 <template>
-  <div>hello world</div>
-</template>
+  <div>
+    <h1>Resume / CV & Cover Letter</h1>
+    <p>In this section, Go Tritons will walk you through an important step of your job application process- Resume, CV,
+      and Cover Letter building. For resume and the CV, we will explain the differences. And then we will break down the
+      specific components for all of them. Let’s start!</p>
 
+
+    <hr>
+
+
+    <div>
+      <h2>Resume vs. CV</h2>
+      <hr>
+      <img src="../assets/resumeVScv.png" width="859px" alt="resume VS. CV">
+
+    </div>
+    <hr>
+    <div>
+      <h2>Resume Breakdown</h2>
+      <hr>
+      <div>
+        <h3>Resume Components</h3>
+
+        <div>
+          <img src="../assets/resumeheader.png" width="493px" alt="header">
+          <p>------> Header: Contact Information</p>
+        </div>
+
+        <div>
+          <img src="../assets/resumesummary.png" width="493px" alt="header">
+          <p>------> Summary<br>
+            Summarize your greatest achievement/character traits to indicate why you are qualified for the position
+            you are applying. See examples at the bottom.</p>
+        </div>
+
+        <div>
+          <img src="../assets/resumeeducation.png" width="493px" alt="header">
+          <p>------> Education<br>
+            Include your School Name, Majors & Minors, Timeline, Expected Graduation Date, and GPA. </p>
+        </div>
+
+        <div>
+          <img src="../assets/resumeexperience.png" width="493px" alt="header">
+          <p>------> Experience<br>
+            This part should include any professional experience that can prove your qualifications for the position
+            you are applying. Start with the most relevent one. If you want to include projects, you can start another section by following the same template.</p>
+        </div>
+
+        <div>
+          <img src="../assets/resumeskill.png" width="493px" alt="header">
+          <p>------> Skills & Activities <br>
+            Include any relevent soft/hard skills/languages/certification/awards that can add to your competences<br>
+            See examples at the bottom</p>
+        </div>
+
+        <a href="../assets/resumetemplate.docx"><button>Download Template</button></a>
+      </div>
+
+
+      <div>
+        <h3>Resume Essentials:</h3>
+        <img src="../assets/resumeessentials.png" width="949px">
+      </div>
+
+      <div>
+        <h3>Sample Resumes</h3>
+        <img src="../assets/sampleresumes.png" width="950px">
+      </div>
+
+      <div>
+        <h3>Polish Your Resume</h3>
+        <p>Now that you have built your Resume, let’s use some Action Verbs to show your skills! </p>
+        <img src="../assets/verb1.png" width="500px">
+        <img src="../assets/verb2.png" width="500px">
+        <br>
+
+        <p>Accomplishment/Impact Statements</p>
+        <img src="../assets/impactStatement.png" width="965px">
+
+        <hr>
+        <p>Example:</p>
+        <ul>
+          <li>Led (verb) a team of 10 volunteers in planning, preparing, and promoting a charity table quiz (description
+            of the work) which raised 15% (quantified impact) of the total revenue.</li>
+        </ul>
+
+        <p>Sample Sentence Structures:</p>
+        <ul>
+          <li>Launched xx in xxx, resulting in xx</li>
+          <li>Developed xx in xxx, helped/raised/changed xxx</li>
+          <li>Analyzed xx and created xx to present to xx at weekly meetings.</li>
+          <li>Launched xx in xxx, resulting in xx</li>
+          <li>Implemented a new xx system that streamlined the xx and allowed xx more efficiently.</li>
+          <li>Coordinated with the xx team to form a xx group for xx that resulted in xx</li>
+        </ul>
+
+
+      </div>
+
+    </div>
+    <hr>
+    <div>
+      <h2>CV Breakdown</h2>
+      <hr>
+      <div>
+        <h3>CV Components</h3>
+        <div>
+          <img src="../assets/cvappendix.png">
+        </div>
+
+        <div>
+          <img src="../assets/cvheader.png">
+        </div>
+
+        <div>
+          <img src="../assets/cveducation.png">
+        </div>
+
+        <div>
+          <img src="../assets/cvresearchinterst.png">
+        </div>
+
+        <div>
+          <img src="../assets/cvresearchappointment.png">
+        </div>
+
+        <div>
+          <img src="../assets/cvscholarship.png">
+        </div>
+
+        <div>
+          <img src="../assets/cvarticle.png">
+        </div>
+
+        <div>
+          <img src="../assets/cvresearchskill.png">
+        </div>
+
+        <div>
+          <img src="../assets/cvservice.png">
+        </div>
+
+        <div>
+          <img src="../assets/cvinportantcourse.png">
+        </div>
+
+        <div>
+          <img src="../assets/cvreference.png">
+        </div>
+
+        <p>Optional Sections to Include (if you have one)</p>
+        <ul>
+          <li>Presentations: include the author(s) and the title of presentation, the conference/symposium name,
+            the location and the date. </li>
+          <li>TEACHING EXPERIENCE: describe your role; include the name of the course and institution, the position
+            you held (e.g., Teaching Assistant, Instructor of Record), the location, and month-year range of the
+            teaching engagement.</li>
+          <li>PROFESSIONAL MEMBERSHIPS OR AFFILIATIONS</li>
+          <li>Certifications & Licensure</li>
+          <li>SKILLS</li>
+          <li>See formatting examples at the bottom.</li>
+        </ul>
+      </div>
+    </div>
+
+    <hr>
+    <div>
+      <h2>Cover Letter Breakdown</h2>
+      <hr>
+      <div>
+        <h3>Cover Letter Components</h3>
+        <img src="../assets/coverletter.png" width="890px">
+      </div>
+    </div>
+
+    <hr>
+    <div>
+      <h2>More Resources</h2>
+      <hr>
+      <p>Resume:</p>
+      <ul>
+        <li>Free Online Resume Builder: Resume Builder</li>
+        <li>Resume Summary Examples: Professional Resume Summary Examples (25+ Statements)</li>
+        <li>Resume Skills Examples: Best Skills for Your Resume (Examples + How to List)</li>
+        <li>Career Center: Create a Resume</li>
+      </ul>
+      <p>CV:</p>
+      <ul>
+        <li>More examples for CV formatting: Curriculum Vitae (CV) Format: Samples & Formatting Tips</li>
+        <li>Cover Letter:</li>
+        <li>Paraphrasing Tool: Paraphrasing Tool</li>
+      </ul>
+      <p>More Articles for tips:</p>
+      <ul>
+        <li>Tips on quantifying your accomplishments</li>
+        <li>4 Ways to Make Sure Your Resume Is Selling You as the One for the Job</li>
+      </ul>
+    </div>
+
+  </div>
+</template>
+<style>
+
+</style>
 <script>
 export default {
   name: "Resume"
