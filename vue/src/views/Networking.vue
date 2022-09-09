d380a4b1c3781ccbd7218b8f09466d92a12b42e5
HuanerW <97192419+HuanerW@users.noreply.github.com>
2022-09-09 14:45:13 -0700

    Add files via upload

---
+++ vue/src/views/Networking.vue
@@ -0,0 +1,365 @@
+<template >
+  <div>
+  <div>
+    <h1>Networking</h1>
+
+    <p class="normal">“New Survey Reveals 85% of All Jobs are Filled Via Networking”</p>
+    <p id="opening">Networking is the process of making connections and building relationships for attain new information and opportunities.
+      It plays an essential part in your career-finding process. But many of you, who are introverts, can feel frustrated about it.
+      We will guide you through this process by providing you several convenient solutions to networking. Let’s start!</p>
+
+    <h2>Why Networking?</h2>
+
+    <div>
+      <img src="../assets/WhyNetWorking.svg" width="650px">
+      <p id="opening">Professional networking involves making connections to attain new career-development information, expertise, and opportunities.
+        It is critical to your career success. However, many of you who are introverts may feel frustrated about it and even avoid it. But don’t worry, we will walk you  you through the process by offering solutions to typical networking routes.
+        Let’s get started!</p>>
+    </div>
+
+    </div>
+
+    <div>
+    <h2>Route One: Networking through Online Platforms</h2>
+
+    <h3>Why use online platforms?</h3><img src="../assets/whyOnlinePlat.svg">
+      <p class="online">2022 Job Seeker Nation reported that 42% say they find out about job openings through social media platforms.</p>
+    </div>
+
+
+
+  <div>
+    <hr>
+    <h3>Common Online Platforms</h3>
+    <div>
+
+      <p class="Question">Question Types</p>
+
+      <span>Define Yourself</span>
+      <span>Behavioral</span>
+      <span>Technical</span>
+
+    </div>
+    <hr>
+  </div>
+
+
+
+
+  <div>
+  <h1>LinkedIn</h1>
+    <div>
+    <img src="../assets/Linkedinin.svg" align="left" >
+    </div>
+    <div>
+      <div class="one">
+      <p> LinkedIn is the world’s largest professional networking platform with over 300 million users and growing.
+        Employers, professors, alumnis, and job-seekers like you are all active on the platform. </p>
+
+      <p> On LinkedIn, you can:</p>
+      <ul>
+        <li>Build your professional presence</li>
+        <li>Form connections</li>
+        <li>Apply to jobs</li>
+        <li>Gather industry information</li>
+      </ul>
+      </div>
+
+      <h4>Go Tritons will break down several important steps for you to network more easily on LinkedIn:</h4>
+      <p id="step1">Step 1</p>
+      <p id="step2"><strong>Build a Professional & Attracting Profile: </strong></p>
+      <p id="opening">A professional profile not only attracts your peers for easier network expansion, but it also draws the attention of industry insiders, particularly recruiters.
+        They will stay longer on your profile, to be more impressed with you, and learn more about your professional side, thus providing job opportunities for you.
+        Go Tritons will walk you through each part in building an attracting LinkedIn Profile.</p>
+      <img src="../assets/Reminder.svg" align="center">
+
+      <div class="row">
+        <div class="column">
+          <img src="../assets/amyChen1.svg" width="500px">
+          <img src="../assets/amyChen2.svg" width="500px">
+          <img src="../assets/amyChen3.svg" width="500px">
+          <img src="../assets/amyChen4.svg" width="500px">
+          <img src="../assets/amyChen5.svg" width="500px">
+        </div>
+      </div>
+      <img src="../assets/learnFromOthers.svg" width="738px" height="242px">
+
+    </div>
+
+
+
+    <div>
+      <p id="step1">Step 2</p>
+      <p id="step2"><strong>Expand Your Professional Network </strong></p>
+      <p></p>
+      <img src="../assets/numberMatters.svg" width="659px" >
+      <p style="width: 830px">However, since recruiters evaluate your connections based on their quality & relevancy,
+        your focus when expanding your network should be on the people that can add to your professional value. </p>
+      <img src="../assets/connectWith.svg" >
+      <h4>Tips</h4>
+    </div>
+
+
+  </div>
+
+   <div class="two">
+     <p> Learn from others:</p>
+     <li>Browse other people's profile pages for inspiration, such as those who have worked in your favorite company and held similar employment as you.</li>
+     <li> Use the LinkedIn Alumni tool to check out the data of your alumni—where they work and live, especially people who are in the same industry as you.</li>
+     <p>Be yourself:</p>
+     <li>Avoid words like “specialized”, “passionate”, and “experienced” since they're overused on LinkedIn. Think about how you stand out and use words that characterize you.</li>
+
+   </div>
+
+
+<div>
+  <p><strong>Expand Your Professional Network </strong></p>
+  <div>
+    <img src="../assets/numberMatters.svg" >
+  </div>
+  <div>
+    <img src="../assets/connectWith.svg" >
+  </div>
+
+</div>
+
+    <div>
+      <hr>
+      <h3>Tritons Connect</h3>
+      <img src="../assets/tritonConnect1.svg" >
+      <img src="../assets/tritonConnect2.svg" >
+      <p id="tritonsC">Highlight</p>
+      <img src="../assets/tritonsConnect1.svg" width="914px" height="314px">
+    </div>
+
+    <div>
+      <hr>
+      <img src="../assets/otherSocialMedia.svg" >
+      <hr>
+    </div>
+
+    <div>
+      <h4>How to network online?</h4>
+      <p>It can be frustrating to reach out. But don't worry; we’ve broken down every step of online networking for you.</p>
+      <p id="step1" >Step 1</p>
+      <p id="step2"><strong>Send an outreach email</strong></p>
+      <p></p>
+      <img src="../assets/netWorkOnline1.svg" style="padding: 20px">
+      <p></p>
+      <p id="step1" >Step 2</p>
+      <p id="step2"><strong>Conduct Information Interview</strong></p>
+      <p></p>
+      <p>Informational interview allows you to learn more deeply about career-related topics,
+        such as career path, industry, or a specific firm, by speaking directly with an experienced
+        individual who has expertise and knowledge in your desired field.
+      </p>
+      <img src="../assets/netWorkOnline2.svg" style="padding: 20px">
+      <p></p>
+      <p id="step1" >Step 3</p>
+      <p id="step2"><strong>Follow up with a Thank-you Note</strong></p>
+      <p>You just had a terrific interview—the person was pleased to meet you, answered all your questions, and learned about you as a person.
+        Now send a thank-you message to appreciate them for meeting with you and to leave a strong & positive impression so they'll consider you when future opportunities arise.
+        You can also recap your abilities and expertise or share your highlighted work in this follow-up letter.</p>
+      <img src="../assets/CareerAdvisory.svg" style="padding: 20px">
+      <p></p>
+      <p id="step1" >Step 4</p>
+      <p id="step2"><strong>Stay in Contact</strong></p>
+      <p>If you don't put effort to keep in touch with the person you asked for help online,
+        you may lose this connection. However, it is critical to maintain contact in order to forge stronger relatinoship that can lead to more opportunities. </p>
+      <img src="../assets/stayInContact.svg" style="padding: 20px">
+
+    </div>
+
+    <div>
+      <h2>Route Two: Networking On Campus</h2>
+      <h4>Why networking on campus?</h4>
+      <p id="opening">As a UCSD student, there are many networking opportunities you can earn on campus.
+        Professional clubs, social sororities and fraternities, on-campus jobs, student media, etc, are all where you can interact with not only your peers, but also guest lecturers, faculty advisors, and mentors.
+      </p>
+      <hr>
+      <h3 style="padding: 50px">Where to network on campus?</h3>
+      <hr>
+      <h4 >Student Organizations</h4>
+      <img src="../assets/studentOrg1.svg" style="padding: 20px">
+      <img src="../assets/studentorg2.svg" style="padding: 20px">
+      <h4 style="padding: 50px">Mentorship Programs</h4>
+
+
+    </div>
+
+
+    <div>
+      <h2>Route Three: Networking in Everyday Routine</h2>
+      <p>Networking with faculty and peers in your field allows you to learn about important news and upcoming changes in your field. Furthermore,
+        communicating with others can help you feel more confident about your career path.</p>
+    </div>
+
+    <div>
+      <h2>How to Maintain Networks?</h2>
+      <p id="Maintain">How to Maintain Networks</p>
+    </div>
+
+    <div>
+      <h2>Networking platforms Summary</h2>
+    </div>
+
+
+  </div>
+
+
+
+
+
+</template>
+
+<script>
+export default {
+  name: "Networking"
+}
+</script>
+
+
+
+<style scoped>
+.scrollbar-demo-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  margin: 10px;
+  text-align: center;
+  border-radius: 4px;
+  background: var(--el-color-primary-light-9);
+  color: var(--el-color-primary);
+}
+#downloadTemplate{
+  border-radius: 0.5rem;
+  padding: 0.5rem 1rem;
+  background-color: #1E4460;
+  border: hidden;
+  color: white;
+}
+#downloadTemplate:hover{
+  background-color: #F4CE71;
+  color: darkblue;
+  border: hidden;
+}
+div{
+  text-align: center;
+}
+h1, h2, h3, h4{
+  text-align: left;
+}
+
+h3{
+  margin-bottom: 50px;
+  font-family: Inter;
+  font-weight: 700;
+  font-size: 16px;
+  font-style: normal;
+}
+h1{
+  margin-bottom: 100px;
+  color: #1E4460;
+  font-weight: 700;
+  font-size: 26px;
+  font-family: "Work Sans";
+}
+h2{
+  font-family: "Work Sans";
+  font-style: normal;
+  font-size: 18px;
+  font-weight: 700;
+  color: #182B49;
+  margin-bottom: 82px;
+  margin-top: 95px;
+  border-top: 1px solid black;
+  border-bottom: 1px solid black;
+  padding-top: 15px;
+  padding-bottom: 15px;
+}
+#opening{
+  font-family: Inter;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 19px;
+  text-align: left;
+  margin-right: 13vw;
+  margin-left: 13vw;
+  padding: 30px;
+}
+#Maintain{
+  width:300px;
+  border-radius: 20px;
+  background-color: cadetblue;
+  text-align: center;
+
+
+}
+p.normal{
+  border: 2px solid red;
+  border-radius: 20px;
+  padding: 30px;
+
+  height:100px;
+}
+p.online{
+  border: 2px solid red;
+  border-radius: 20px;
+  padding: 20px;
+  width:600px;
+  margin-left: 13vw;
+
+}
+p.Question{
+  width:300px;
+  margin-left: 25vw;
+  border: 2px solid red;
+  border-radius: 50px;
+  padding: 20px;
+}
+#step1{
+  border-radius: 50px;
+  background-color: #F4CE71;
+  width: 60px;
+  display:inline-block;
+  margin-right:10px;
+
+}
+#step2{
+  display:inline-block;
+}
+#tritonsC{
+  border-radius: 50px;
+  background-color:yellow;
+  width: 60px;
+  margin-left:40px;
+}
+.one{
+  border-style: dotted;
+  border-radius: 30px;
+  width:600px;
+  border-color: cadetblue;
+}
+.two{
+  text-align: center;
+  border-radius: 30px;
+  width:600px;
+
+}
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+.column {
+  flex: 50%;
+  padding: 0 4px;
+}
+.column img {
+  margin-top: 8px;
+  vertical-align: middle;
+}
+
+</style>
